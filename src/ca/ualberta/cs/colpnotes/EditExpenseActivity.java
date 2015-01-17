fbc95b00e3d1e924ffeadbe319e5242bc7628f8f
Elliot Colp <thelastbanana@gmail.com>
2015-01-17 16:05:48 -0700

    Added expense edit activity.

---
+++ src/ca/ualberta/cs/colpnotes/EditExpenseActivity.java
@@ -0,0 +1,31 @@
+package ca.ualberta.cs.colpnotes;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class EditExpenseActivity extends Activity {
+	private View actionBarView;
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        actionBarView = getLayoutInflater().inflate(R.layout.menu_edit_expense, null);
+        ActionBar actionBar = getActionBar();
+        actionBar.setCustomView(actionBarView);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+        getMenuInflater().inflate(R.menu.edit_expense, menu);
+        
+        return true;
+    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_expense);
+	}
+
+}
