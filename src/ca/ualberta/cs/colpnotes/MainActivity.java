4807ea80bb078c3572baf1f4be6bc01e7a7bff7e
Elliot Colp <thelastbanana@gmail.com>
2015-01-17 19:57:19 -0700

    Added Claim list and started on navigation to other activities.

--- src/ca/ualberta/cs/colpnotes/MainActivity.java
+++ src/ca/ualberta/cs/colpnotes/MainActivity.java
@@ -1,19 +1,40 @@
 package ca.ualberta.cs.colpnotes;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+	private ClaimAdapter claimListAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Get the global claims from the singleton
+        ArrayList<Claim> claims = ClaimListController.getClaimList().getClaims();
+        
+        // Create the adapter
+        claimListAdapter = new ClaimAdapter(this, R.layout.claim_list_item, claims);
+        
+        // Attach to the ListView 
+        ListView listView = (ListView) findViewById(R.id.claim_list_view);
+        listView.setAdapter(claimListAdapter);
     }
 
+    public void addClaim() {
+    	Intent intent = new Intent(MainActivity.this, EditClaimActivity.class);
+    	startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,8 +49,9 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_add_claim) {
+        	addClaim();
+        	return true;
         }
         return super.onOptionsItemSelected(item);
     }
